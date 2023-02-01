74e529c38978c75e48059b7f187956b24d5d6f63
subhajitpatra498 <subhajitpatra498@gmail.com>
2023-02-02 02:50:28 +0530

    customer signup and signin

---
+++ Readme.md
@@ -0,0 +1,74 @@
+# **Restaurant**:
+> ### restaurant_signup ***(sudip) done***
+- id
+- username
+- name
+- email
+- phone number
+- address
+- password\
+(show signup successful)
+> ### restaurant_signin ***(sudip) done***
+  - email
+  - password
+  (show signed in successfully)
+ >### add_items ***(Tuhin)***
+  - item name
+  - item description
+  - item price
+  - item cooking time
+  - item quantity
+  - ForeignKey('restaurant_signup.id')
+    (items added successfully)
+> ### get_customer_ordered_dishes ***(Tuhin)***
+  - restaurant_id\
+  (show order details of current restaurant)
+
+# **User**:
+> ### customer_signup ***(subhajit)***
+  - id
+  - name
+  - email
+  - phone number
+  - address
+  - password\
+  (signup successful)
+>### customer_signin ***(Rounak)***
+  - username
+  - password\
+  (signed in successfully)
+> ### choose_restaurant ***(sayan)***
+  - restaurant_name\
+  (if restaurant_name not match --> restaurant not available)\
+  (if restaurant_name matched --> show available dishes and its rating)
+> ### choose_dishes_place_order ***(subhajit)***
+  - restaurant_name
+  - dish_name
+  - quantity
+  - delivery_address
+  - ForeignKey('customer_signup.id')
+  
+  (if quantity not available then show the available quantity) \
+  (if quantity available show dishes are available.)\
+  (Order successful. Payment should be done in cash to the delivery person.)
+> ### check ordered history of customers
+  - username
+  >show dictionary of oredered history of customers where keys are 
+  ```
+  >- restaurant_name
+  >- dish_name
+  >- quantity
+  >- price
+  >- Total_price
+  >- delivery_address
+  ```
+> ### delivery
+- restaurant_name\
+  (it will be deleted the first order from the customer order table of the specific restaurant and show it's delivered order successfully)
+> ###  give_rating_feedback ***(sayan)***
+  - restaurant_name
+  - customer_name
+  - item_name
+  - item_rating
+  - item_feedback \
+  (Thanks for your feedback)
---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from config import db, SECRET_KEY
+from os import environ, path, getcwd
+from dotenv import load_dotenv
+from datetime import datetime,date
+import pytz
+from sqlalchemy.orm import sessionmaker
+from model.customer import Customer
+# from model.add_items import Add_items
+# from model.customer_ordered_record import Customer_ordered_record
+# from model.get_customer_ordered_dishes import Get_customer_ordered_dishes
+
+load_dotenv(path.join(getcwd(),'.env'))
+
+def create_app():
+    app=Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"]=environ.get('DB_URI')
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = False
+    app.secret_key = SECRET_KEY
+    db.init_app(app)
+    print("DB Initialized Sucessfully")
+    CORS(app)
+    with app.app_context():
+        #  customer signup part
+        @app.route('/customer_signup', methods=['POST'])
+        def customer_signup():
+            data = request.form.to_dict(flat=True)
+            print(data)
+            new_customer = Customer(
+                username=data["username"],
+                password=data['password'],
+                name=data['name'],
+                email=data['email'],
+                phone=data['phone'],
+                address=data['address']
+            )
+            try:
+                users=Customer.query.all()
+                usernamelist=[user.username for user in users]
+                if new_customer.username  not in usernamelist:
+                    db.session.add(new_customer)
+                    db.session.commit()
+                else:
+                    return jsonify("username already in used")
+            except:
+                return jsonify("some thing went wrong")
+            return jsonify(msg="Signup Successfully")
+# customer signin part
+        @app.route("/customer_signin", methods=['POST'])
+        def customer_signin():
+            if request.method=="POST":
+                    c_username=request.form['username']
+                    c_password=request.form['password']
+                    users=Customer.query.all()
+                    for user in users:
+                        if user.username==c_username and user.password==c_password :
+                            user_idx=user.id
+                            response={
+                                "massage":"Login successful",
+                                "user_id": user_idx
+                            }
+                            return response
+                        else:
+                            continue
+            return "user doesn't exists"
+
+        db.create_all()
+        db.session.commit()
+        return app
+
+if __name__ == "__main__":
+    app=create_app()
+    app.run(host='0.0.0.0',port=4545,debug=True)
