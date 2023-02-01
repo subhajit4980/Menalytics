dac8080163e734f5986dd64ede5cde11f4ab2018
subhajitpatra498 <subhajitpatra498@gmail.com>
2023-02-02 02:11:32 +0530

    repair

--- config.py
+++
@@ -1,9 +0,0 @@
-from os import environ, path, getcwd
-from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
-
-load_dotenv(path.join(getcwd(), '.env'))
-
-SECRET_KEY = environ.get('SECRET_KEY')
-
-db = SQLAlchemy()
--- model/customer_ordered_record.py
+++
@@ -1,5 +0,0 @@
-from config import db
-
-class Customer_ordered_record(db.Model):
-    __tablename__ = 'customer_ordered_record'
-    
